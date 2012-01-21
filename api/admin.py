ad710d474b45efdde57549786451386bd601c01b
Alberto Ruiz <aruiz@gnome.org>
2012-01-21 02:12:01 +0000

    Added admin interface for api classes

---
+++ api/admin.py
@@ -0,0 +1,15 @@
+from api.models import *
+from django.contrib import admin
+
+admin.site.register(Symbol)
+admin.site.register(Class)
+admin.site.register(Delegate)
+admin.site.register(Method)
+admin.site.register(Interface)
+admin.site.register(Enum)
+admin.site.register(EnumValue)
+admin.site.register(Bitfield)
+admin.site.register(BitfieldValue)
+admin.site.register(BuiltIn)
+admin.site.register(Constant)
+
