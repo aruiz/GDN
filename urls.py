a8053bb36cf9cfe292274b79c435ab6eb6fc6633
Alberto Ruiz <aruiz@gnome.org>
2012-01-21 01:53:33 +0000

    Fix url include for api app

--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+# vim: tabstop=4 noexpandtab shiftwidth=4 softtabstop=4
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -8,7 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gnome_developer_network.views.home', name='home'),
     # url(r'^gnome_developer_network/', include('gnome_developer_network.foo.urls')),
-    url(r'^api/', include('gnome_developer_network.api.home'))
+    url(r'^api/', include('gnome_developer_network.api.urls'))
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
