2c8a5921a7fe7ad9a9be5547cb02533eaf4796b3
Alberto Ruiz <aruiz@gnome.org>
2012-03-12 17:04:50 +0000

    Use relative path for templates

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+import os.path
 # Django settings for gnome_developer_network project.
 
 DEBUG = True
@@ -103,9 +104,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join (os.path.dirname(__file__), 'templates').replace('\\', '/'),
 )
 
 INSTALLED_APPS = (
